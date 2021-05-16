2756e406c6efd8924bc7480dc8c3d98852c9773a
Zoltan Busi <oma.spencer@gmail.com>
2021-05-16 18:46:55 +0200

    Login, Register, Logout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="home">
     <h1>Welcome to the App!</h1>
+    <p v-if="!loggedIn">
+      To use this app you'll need to
+      <router-link to="/login">Login</router-link>
+      or
+      <router-link to="/register">Register</router-link>
+    </p>
   </div>
 </template>
 
 <script>
-export default {}
+import { authComputed } from '../vuex/helper'
+export default {
+  computed: {
+    ...authComputed
+  }
+}
 </script>
