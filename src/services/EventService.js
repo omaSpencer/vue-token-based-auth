ffc0eb1907eb866ebe432a4b5ed65dc628187512
Zoltan Busi <oma.spencer@gmail.com>
2021-05-16 17:03:19 +0200

    init

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
-  withCredentials: false,
+  baseURL: `http://localhost:3000`,
+  withCredentials: false, // This is the default
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
@@ -10,10 +10,13 @@ const apiClient = axios.create({
 })
 
 export default {
-  getEvents() {
-    return apiClient.get('/events')
+  getEvents(perPage, page) {
+    return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
   },
   getEvent(id) {
     return apiClient.get('/events/' + id)
+  },
+  postEvent(event) {
+    return apiClient.post('/events', event)
   }
 }
